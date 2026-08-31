b65fa2455966786221b2108595aac0d2f28a6381
morettitommaso <tommasomoretti.ba@gmail.com>
2026-08-31 13:45:25 +0200

    newlogo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
           <div>
             <div className="flex items-center gap-2 font-display text-base text-ink">
               <Image
-                src="/logo.png"
+                src="/logo5.png"
                 alt="BMLA"
                 width={24}
                 height={24}
